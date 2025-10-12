fc1a755e2a39594091515bef81cf6d2abfbdd276
Dennis Ballance <dwballance@gmail.com>
2025-10-12 15:03:23 -0700

    Added Dusty Strings and error checking. Improved offset spacing parameter.

---
+++ config.js
@@ -0,0 +1,12 @@
+// Configuration file for Dulcimer Chord Finder
+// This file contains URLs and other configuration settings that may need to be updated
+
+const config = {
+    // Google Form URL for requesting new instrument layouts
+    googleFormUrl: "https://docs.google.com/forms/d/e/1FAIpQLSdiFmni2nnSVDc_xM-JiW5cav6RqslKIAf4dia1EV1u9_K0oA/viewform?usp=dialog",
+    
+    // Other configuration settings can be added here as needed
+    // For example:
+    // githubIssuesUrl: "https://github.com/dwballance/Dulcimer-Chord-Finder/issues/new",
+    // supportEmail: "support@example.com"
+};
